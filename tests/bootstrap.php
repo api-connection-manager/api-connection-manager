0c70a90f354fa9acd9c1195b68695211940b05c5
daithi-coombes <webeire@gmail.com>
2013-07-18 22:22:15 +0100

    phpunit tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+define( 'WP_TESTS_DIR', getenv( 'WP_TESTS_DIR' ) );
+if( !file_exists( WP_TESTS_DIR  ) )
+	throw new Exception( 'Unable to find tests directory. Please set the WP_TESTS_DIR environment variable');
+
+require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../api-connection-manager.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
