5ec96d1c1c5659bbadf2d8295bb9595864076f6a
daithi-coombes <webeire@gmail.com>
2013-07-22 20:39:02 +0100

    document API_Con_Consumer

--- lib/class-api-con-consumer.php
+++ lib/class-api-con-consumer.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * File for the API_Con_View class
+ */
 
+/**
+ * Class for handling all view actions, such as login forms etc.
+ * @package  api-connection-manager
+ * @todo  integrate API_Con_View with bootstrap
+ * @author Daithi Coobmes <webeire@gmail.com>
+ */
 class API_Con_Consumer{
 	
+	/**
+	 * Construct.
+	 * @param API_Con_Service $service The service to build the consumer around.
+	 */
 	function __construct( API_Con_Service $service ){
 
 		switch( $service->auth_type ){
@@ -19,6 +32,11 @@ class API_Con_Consumer{
 		}
 	}
 
+	/**
+	 * Factory method. Builds a new API_Con_Consumer
+	 * @param  API_Con_Service $service The service to build the consumer around.
+	 * @return API_Con_Consumer
+	 */
 	public static function get_consumer( API_Con_Service $service ){
 		return new API_Con_Consumer( $service );
 	}
