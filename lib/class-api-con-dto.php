d4e045e18d78398d556f8072da7aa02109af1381
daithi-coombes <webeire@gmail.com>
2013-07-23 11:23:03 +0100

    update docs

--- lib/class-api-con-dto.php
+++ lib/class-api-con-dto.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Class file for API_Con_DTO
+ */
 
+/**
+ * Data Transport Object for handling responses from remote services.
+ * This class can be mocked on client side and used as if it came from
+ * a remote service.
+ * @package  api-connection-manager
+ * @author Daithi Coobmes <webeire@gmail.com>
+ */
 class API_Con_DTO{
 	
+	/** @var array The data passed to the server */
 	public $data;
 
+	/**
+	 * Construct
+	 * @param array $data An array of key value pairs passed from the remote server
+	 */
 	function __construct( array $data=null ){
 
 			$this->data = $data;
